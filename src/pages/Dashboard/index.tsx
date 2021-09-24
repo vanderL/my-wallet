12ead558aa40bce81b7adcc8360f0a101f0c88d4
vanderL <vnd.vander@gmail.com>
2021-09-24 14:31:57 -0300

    Structure init

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,5 @@
+export const Dashboard: React.FC = () => {
+    return (
+        <div>Dashboard</div>
+    )
+}
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,5 @@
+export const SingIn: React.FC = () => {
+    return (
+        <div>SingIn</div>
+    )
+}
