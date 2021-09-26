bffec33cb40e433d28a5a11d63d9cd1abadb0fc7
vanderL <vnd.vander@gmail.com>
2021-09-26 20:42:41 -0300

    Creating Layout Aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,51 @@
-import { Container } from "./styles"
+import logoImg from '../../assets/logo.svg';
+
+import { 
+    MdDashboard, 
+    MdArrowDownward, 
+    MdArrowUpward, 
+    MdExitToApp 
+} from 'react-icons/md'
+
+import { 
+    Container, 
+    Header, 
+    LogoImg, 
+    MenuContainer,
+    MenuItemLink,
+    Title
+} from "./styles"
+
 
 export const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogoImg src={logoImg} alt="My Wallet" />
+                <Title>My Wallet</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowDownward />
+                    Entradas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowUpward />
+                    Saidas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdExitToApp />
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
     )
 }
