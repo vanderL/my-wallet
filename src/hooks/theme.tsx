c1106ca34a9e01c4fa8fbefdafa7312104b4dd68
vanderL <vnd.vander@gmail.com>
2021-10-18 15:21:03 -0300

    Saving the theme to local storage

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -29,13 +29,24 @@ interface ITheme {
 const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
 const ThemeProvider: React.FC = ({ children }) => {
-    const [theme, setTheme] = useState<ITheme>(light);
+    const [theme, setTheme] = useState<ITheme>(() => {
+        const themeSaved = localStorage.getItem('@my-wallet:theme');
+
+        if(themeSaved) {
+            return JSON.parse(themeSaved);
+        } else {
+            return light;
+        }
+    });
 
     const toggleTheme = () => {
         if(theme.title === 'dark') {
             setTheme(light);
+            localStorage.setItem('@my-wallet:theme', JSON.stringify(light));
         } else {
             setTheme(dark);
+            localStorage.setItem('@my-wallet:theme', JSON.stringify(dark));
+
         };
     };
 
